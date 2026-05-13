c749ef04434439f57d65e7bdda5b7058cb876dd9
Drew Meyers <120533968+giraffeTreePruner@users.noreply.github.com>
2026-05-12 20:42:17 -0700

    Spring boiler

---
+++ src/main/java/com/codedgiraffe/smartvolt/SmartvoltApplication.java
@@ -0,0 +1,13 @@
+package com.codedgiraffe.smartvolt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartvoltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartvoltApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codedgiraffe/smartvolt/SmartvoltApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codedgiraffe.smartvolt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartvoltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
